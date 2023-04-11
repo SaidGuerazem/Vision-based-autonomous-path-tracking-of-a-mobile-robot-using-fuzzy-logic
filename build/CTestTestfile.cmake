7664894c1a803bb55d9711adc3f8bf7f2afcd952
Said <said.guerazem@g.enp.edu.dz>
2023-04-11 01:43:59 +0100

    creating the ws

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/arikakite/git_repos/ws/src
+# Build directory: /home/arikakite/git_repos/ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
